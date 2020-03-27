42761c0ad4347b10dcc4fc4ed163c5030f26b13e
fn <3456104398@qq.com>
2020-03-27 21:42:37 +0800

    last

--- js/index.js
+++ js/index.js
@@ -24,37 +24,61 @@
 
 // 获取所有格子
 let imgs = document.querySelectorAll("img");
+let reset = document.querySelector(".reset");
+let resetAlart = document.querySelector(".alert");
+let mask = document.querySelector("#mask");
+let scroeNum = document.querySelector('.scroeNum');
+let jump = document.querySelector('#jump');
+let jumpBtn = jump.querySelector("button");
 
-window.addEventListener("keydown",function({keyCode}){
-    switch(keyCode){
-        case 37:
-            run([0, 1, 2, 3]);
-            run([4, 5, 6, 7]);
-            run([8, 9, 10, 11]);
-            run([12, 13, 14, 15]);
-            break;
-        case 38:
-            run([0, 4, 8, 12]);
-            run([1, 5, 9, 13]);
-            run([2, 6, 10, 14]);
-            run([3, 7, 11, 15]);
-            break;
-        case 39:
-            run([3, 2, 1, 0]);
-            run([7, 6, 5, 4]);
-            run([11, 10, 9, 8]);
-            run([15, 14, 13, 12]);
-            break;
-        case 40:
-            run([12, 8, 4, 0]);
-            run([13, 9, 5, 1]);
-            run([14, 10, 6, 2]);
-            run([15, 11, 7, 3]);
-            break; 
+// 键盘事件
+window.addEventListener("keydown",keyDown);
+
+function keyDown(e){
+    if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
+        switch(e.keyCode){
+            case 37:
+                run([0, 1, 2, 3]);
+                run([4, 5, 6, 7]);
+                run([8, 9, 10, 11]);
+                run([12, 13, 14, 15]);
+                break;
+            case 38:
+                run([0, 4, 8, 12]);
+                run([1, 5, 9, 13]);
+                run([2, 6, 10, 14]);
+                run([3, 7, 11, 15]);
+                break;
+            case 39:
+                run([3, 2, 1, 0]);
+                run([7, 6, 5, 4]);
+                run([11, 10, 9, 8]);
+                run([15, 14, 13, 12]);
+                break;
+            case 40:
+                run([12, 8, 4, 0]);
+                run([13, 9, 5, 1]);
+                run([14, 10, 6, 2]);
+                run([15, 11, 7, 3]);
+                break; 
+        };
+        createValue();
     };
-    createValue()
+};
+
+// window 一上来时生成
+window.addEventListener("DOMContentLoaded",function(){
+    init();
+},{
+    once: true
 });
 
+// 初始化生成
+function init(){
+    let rondom = Math.floor(Math.random() * imgs.length);
+    imgs[rondom].setAttribute('value', 2);
+    imgs[rondom].src = `./img/cube_2.png`;
+}
 
 // 随机生成
 function createValue(){
@@ -64,11 +88,11 @@ function createValue(){
         imgs[rondom].setAttribute('value', 2);
         imgs[rondom].src = `./img/cube_2.png`;
     } else {
-        setTimeout(createValue,20)
+        setTimeout(createValue, 20);
     };
 };
 
-
+// 主要逻辑
 function G2048(arr){
     let newArr = [];
     for(let i = 0; i < arr.length; i++){
@@ -97,21 +121,92 @@ function G2048(arr){
 };
 
 function run(arr){
-    /**
-     * 第一排
-     *  - [0, 1, 2, 3]
-     * 第二排
-     *  - [4, 5, 6, 7]
-    */
+    /****
+     *      第一排          第二排
+     *    [0, 1, 2, 3]  [4, 5, 6, 7]
+    ***/
     let newValue = G2048([
         Number(imgs[arr[0]].getAttribute('value')),
         Number(imgs[arr[1]].getAttribute('value')),
         Number(imgs[arr[2]].getAttribute('value')),
         Number(imgs[arr[3]].getAttribute('value')),
     ]);
-
     for(let i = 0; i < arr.length; i++){
         imgs[arr[i]].setAttribute('value', newValue[i]);
-        imgs[arr[i]].src = `./img/cube_${newValue[i]}.png`
+        imgs[arr[i]].src = `./img/cube_${newValue[i]}.png`;
     };
-}
+
+    // 分数统计
+    let total = 2;
+    imgs.forEach(item=>{
+        if(item.getAttribute("value")){
+            // 游戏到达 2048 
+            if(item.getAttribute("value") >= 2048){
+                jump.style.display = 'block';
+                mask.style.display = 'block';
+                window.removeEventListener("keydown", keyDown);
+            };
+            total += Number(item.getAttribute("value"));
+        };
+    });
+    scroeNum.innerHTML = total;
+};
+
+// 重置游戏
+function resetGame(){
+    imgs.forEach(item=>{
+        item.setAttribute("value", 0);
+        item.src = `./img/cube_0.png`;
+    });
+};
+
+let first = 0; // 重置分数
+
+// 重新开始点击时
+reset.addEventListener("click",function(e){
+    // 显示弹窗
+    resetAlart.style.display = 'block';
+    // 卸载键盘事件
+    if(resetAlart.style.display = 'block'){
+        window.removeEventListener('keydown', keyDown)
+    };
+    mask.style.display = 'block';   // 遮罩层
+    resetAlart.classList.add("alert-acitive");  // 显示弹窗动画
+});
+
+// 完成 2048 小游戏后的重新开始的按钮
+jumpBtn.addEventListener("click",function(){
+    // 关闭弹窗
+    jump.style.display = 'none';
+    mask.style.display = 'none';
+    // 重新游戏
+    resetGame();
+    // 初始化棋子
+    init()
+    scroeNum.innerHTML = first;
+    // 重新执行事件
+    window.addEventListener('keydown', keyDown);
+});
+
+// 弹窗组件
+resetAlart.addEventListener("click",function(e){
+    if(e.target.classList.contains('yes')){
+        // 关闭弹窗、遮罩
+        resetAlart.style.display = 'none';
+        mask.style.display = 'none'
+        // 重置游戏
+        resetGame();
+        // 重置分数
+        scroeNum.innerHTML = first;
+        // 初始化生成
+        init();
+        // 重新执行事件
+        window.addEventListener('keydown', keyDown);
+    } else if(e.target.classList.contains('no')) {
+        // 关闭弹窗、遮罩
+        resetAlart.style.display = 'none';
+        mask.style.display = 'none';
+        // 重新执行事件
+        window.addEventListener('keydown', keyDown);
+    };
+});
